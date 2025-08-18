c5675624b31f03bc5cffc6f451d42fec037d6fdb
HarshalMN <harshalnalawade2020@gmail.com>
2025-08-18 17:52:23 +0530

    Added Restuarantcontroller example to return dummy list of restruarants

--- src/main/java/com/backend/example/entity/RestaurantDetails.java
+++ src/main/java/com/backend/example/entity/RestaurantDetails.java
@@ -1,9 +1,10 @@
 package com.backend.example.entity;
-import java.util.Date;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.Data;
 
+@Data
 @Entity
 @Table(name="restaurant")
 public class RestaurantDetails {
@@ -11,8 +12,7 @@ public class RestaurantDetails {
     private Integer rest_id;
     private String name;
     private String address;
-    private String cuisine;
-    private Date open_time;
-    private Date close_time;
+    private String open_time;
+    private String close_time;
     private Integer no_of_tables;
 }
